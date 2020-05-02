cc35af90a8d6f9053843386a2ec4741e18ba8b40
mohit kumar singh <mohitkumarsingh907@gmail.com>
2020-05-02 16:01:01 +0530

    set name = product id for query selector.

--- compo.php
+++ compo.php
@@ -41,10 +41,10 @@ function cartElement($prdimg,$prdname,$prdprice,$productid,$quant)
 </div>
 <div class=\"col-md-3\" id=\"updown\">
 <div class=\"pt-5\">
-<button type=\"button\" class=\"btn bg-light border rounded-circle\"  ><i class=\"fa fa-minus sub-cart\" name=\"minus\"></i></button>
+<button type=\"button\" class=\"btn bg-light border rounded-circle\"  ><i class=\"fa fa-minus sub-cart\" name=\"$productid\"></i></button>
 <input type=\"text\" value=\"1\" name=\"quantity\"  id=\"$productid\" class=\"form-control w-25 d-inline\">
 <input type=\"hidden\" name=\"product_id\" value=\"$productid\">
-<button type=\"button\"class=\"btn bg-light border rounded-circle\"><i class=\"fa fa-plus add-cart\" name=\"plus\"></i></button>
+<button type=\"button\"class=\"btn bg-light border rounded-circle\"><i class=\"fa fa-plus add-cart\" name=\"$productid\"></i></button>
 
 </div>
 <script src=\"cart1.js\"></script>
