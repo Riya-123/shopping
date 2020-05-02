468fab167a65eddff77bfc9b17d3feeb7d2e575a
mohit kumar singh <mohitkumarsingh907@gmail.com>
2020-05-02 16:03:13 +0530

    use product id as query selector for updating count

--- cart1.js
+++ cart1.js
@@ -1,13 +1,14 @@
-
-let btndd=document.querySelector('#add');
-	let btnSubtract=document.querySelector('#substract');
-	let input=document.querySelector('input');
-	btnSubtract.addEventListener('click',()=>{
+let btnSubtract=document.querySelector('#substract');
+btnSubtract.addEventListener('click',e=>{
+	const productId = e.target.attributes.name
+	let input=document.querySelector('#'+productId);
 	input.value=parseInt(input.value,10)-1;
 });	
 
-
+let btndd=document.querySelector('.add-cart');
 btndd.addEventListener('click',()=>{
+	const productId = e.target.attributes.name
+	let input=document.querySelector('#'+productId);
 	input.value=parseInt(input.value,10)+1;
 });
 
